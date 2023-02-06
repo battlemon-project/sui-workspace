b937499d3c0876b245666f26faa1ff6d4e542665
ChzenChzen <fedorovdanila@me.com>
2023-02-06 19:30:57 +0700

    fix names of structs

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,132 +1,94 @@
-use anyhow::anyhow;
-use async_graphql::{InputObject, InputValueResult, ScalarType, SimpleObject};
+pub mod events;
+pub mod errors;
+
+use async_graphql::{InputObject, SimpleObject};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx_core::types::Json;
 pub use sui_sdk;
-use sui_sdk::rpc_types::{SuiEvent, SuiEventFilter, SuiMoveStruct, SuiMoveValue};
-use sui_sdk::types::base_types::ObjectID;
 
-#[derive(SimpleObject, InputObject, Serialize, Deserialize, Debug)]
+#[derive(SimpleObject, InputObject, Serialize, Deserialize, Debug, Clone)]
 #[graphql(input_name = "TraitInput")]
 pub struct Trait {
     pub name: String,
     pub flavour: String,
 }
 
-#[derive(SimpleObject, InputObject, Serialize, Deserialize, Debug)]
-#[graphql(input_name = "NftTokenInput")]
-pub struct NftToken {
+#[derive(SimpleObject, InputObject, Serialize, Deserialize, Debug, Clone)]
+#[graphql(input_name = "NftInput")]
+pub struct Nft {
     pub id: String,
     pub r#type: String,
     pub owner: String,
     pub url: String,
     pub traits: Vec<Trait>,
+    pub items: Vec<Nft>,
     pub created_at: DateTime<Utc>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct NftTokenSql {
+#[derive(SimpleObject, Serialize, Deserialize, Debug, Clone)]
+pub struct Item {
+    pub lemon_id: String,
+    pub item_id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct NftSql {
     pub id: String,
     pub r#type: String,
     pub owner: String,
     pub url: String,
     pub traits: Json<Vec<Trait>>,
+    pub items: Json<Vec<NftSql>>,
     pub created_at: DateTime<Utc>,
 }
 
-impl From<NftToken> for NftTokenSql {
+impl From<Nft> for NftSql {
     fn from(
-        NftToken {
+        Nft {
             id,
             r#type,
             owner,
             url,
             traits,
+            items,
             created_at,
-        }: NftToken,
+        }: Nft,
     ) -> Self {
+        let items = items.into_iter().map(Into::into).collect();
         Self {
             id,
             r#type,
             owner,
             url,
             traits: Json(traits),
+            items: Json(items),
             created_at,
         }
     }
 }
 
-impl From<NftTokenSql> for NftToken {
+impl From<NftSql> for Nft {
     fn from(
-        NftTokenSql {
+        NftSql {
             id,
             r#type,
             owner,
             url,
             traits,
+            items,
             created_at,
-        }: NftTokenSql,
+        }: NftSql,
     ) -> Self {
+        let items = items.0.into_iter().map(Into::into).collect();
         Self {
             id,
             r#type,
             owner,
             url,
             traits: traits.0,
+            items,
             created_at,
         }
-    }
-}
-
-impl TryFrom<SuiEvent> for NftToken {
-    type Error = anyhow::Error;
-
-    fn try_from(event: SuiEvent) -> Result<Self, Self::Error> {
-        let SuiEvent::MoveEvent { sender,  fields, .. } = event else {
-            return Err(anyhow!("Wrong `SuiEvent`"));
-        };
-
-        let Some(SuiMoveStruct::WithFields(fields)) = fields else {
-            return Err(anyhow!("Wrong `SuiMoveStruct`"));
-        };
-
-        let Some(SuiMoveValue::Address(addr)) = fields.get("id") else {
-            return Err(anyhow!("Wrong `SuiMoveValue`"));
-        };
-
-        let Some(SuiMoveValue::String(url)) = fields.get("url") else {
-            return Err(anyhow!("Wrong `SuiMoveValue`"))
-        };
-
-        let Some(SuiMoveValue::Vector(traits)) = fields.get("traits") else {
-            return Err(anyhow!("Wrong `SuiMoveValue`"));
-        };
-
-        let mut ret_traits = Vec::new();
-        for item in traits {
-            let SuiMoveValue::Struct(SuiMoveStruct::WithTypes { fields, .. }) = item else {
-                continue
-            };
-
-            let Some(SuiMoveValue::String(name)) = fields.get("name").cloned() else {
-                continue
-            };
-
-            let Some(SuiMoveValue::String(flavour)) = fields.get("flavour").cloned() else {
-                continue
-            };
-
-            ret_traits.push(Trait { name, flavour })
-        }
-
-        Ok(NftToken {
-            id: addr.to_string(),
-            r#type: "lemon".to_string(),
-            owner: sender.to_string(),
-            url: url.to_owned(),
-            traits: ret_traits,
-            created_at: Utc::now(),
-        })
     }
 }
