2eff30c222eab60a79188c3b2e7a4531dcb0df07
ChzenChzen <fedorovdanila@me.com>
2023-02-06 20:59:22 +0700

    update sql query in remove_item_db

--- backend/src/graphql.rs
+++ backend/src/graphql.rs
@@ -144,9 +144,10 @@ async fn remove_item_db(
     query!(
         r#"
         UPDATE nfts
-        SET items = (SELECT jsonb_agg(elements)::jsonb 
+        SET items = COALESCE((SELECT jsonb_agg(elements)::jsonb 
                         FROM jsonb_array_elements(items) elements
-                        WHERE elements->> 'id' != $1)
+                        WHERE elements->> 'id' != $1),
+                        '[]'::jsonb)
         WHERE id = $2
         "#,
         item_id,
