17e1807cad474bbac6a46ee222874abf2c0d92c3
ChzenChzen <fedorovdanila@me.com>
2023-02-18 11:41:45 +0700

    - replace context to wrap_err - change error log message

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,4 +1,4 @@
-use eyre::{Context, Result};
+use eyre::{Result, WrapErr};
 use futures::StreamExt;
 use indexer::{config, handle_contract_event, telemetry};
 use models::sui_sdk::{rpc_types::SuiEventFilter, types::base_types::ObjectID, SuiClientBuilder};
@@ -7,15 +7,15 @@ use tracing::{error, info};
 #[tokio::main]
 async fn main() -> Result<()> {
     let subscriber = telemetry::get_subscriber("indexer".into(), "info".into(), std::io::stdout);
-    telemetry::init_subscriber(subscriber).context("Failed to init tracing subscriber")?;
+    telemetry::init_subscriber(subscriber).wrap_err("Failed to init tracing subscriber")?;
     info!("Loading application config");
-    let config = config::load_config().context("Failed to load app config")?;
+    let config = config::load_config().wrap_err("Failed to load app config")?;
     info!("Setup Sui Rust SDK");
     let sui = SuiClientBuilder::default()
         .ws_url(&config.sui_json_rpc.ws_url)
         .build(&config.sui_json_rpc.http_url)
         .await
-        .context("Failed to build SuiClient")?;
+        .wrap_err("Failed to build SuiClient")?;
 
     let contract = config.sui_contract.address.as_str();
     let event_filter = SuiEventFilter::Package(ObjectID::from_hex_literal(contract)?);
@@ -23,12 +23,12 @@ async fn main() -> Result<()> {
         .event_api()
         .subscribe_event(event_filter)
         .await
-        .context("Failed to subscribe to events")?;
+        .wrap_err("Failed to subscribe to events")?;
 
     info!("Start to poll Sui Node for contract `{contract}`");
     while let Some(contract_event) = contract_events.next().await {
         if let Err(e) = handle_contract_event(contract_event, &config).await {
-            error!("While handling contract events obtain error: {e:?}");
+            error!("An error is occurring while I handle contract events. Error: {e:?}");
         }
     }
 
